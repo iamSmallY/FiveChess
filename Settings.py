ad7418763f3accb9f5eb70c93d31b5cc1b52229a
SmallY <2907047490@qq.com>
2019-11-19 15:58:48 +0800

    delete everything again

--- Settings.py
+++
@@ -1,9 +0,0 @@
-BIRDS_IMAGES_LOCATION = [
-    'resources/sprites/bird0_0.png',
-    'resources/sprites/bird0_1.png',
-    'resources/sprites/bird0_2.png'
-]
-BIRDS_WING_SOUND_LOCATION = 'resources/sounds/sfx_wing.wav'
-
-SCREEN_WIDTH = 288
-SCREEN_HEIGHT = 512
